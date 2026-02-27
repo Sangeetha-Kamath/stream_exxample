8950c6168fc46a928e0b0adc1179cbf7a07db25e
Sangeetha Kamath <sangeethakamath1999@gmail.com>
2026-02-27 14:16:11 +0530

    test case issue fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -19,6 +19,6 @@ void main() {
     //show loader initially becuase the stream is not emitted yet
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
     await tester.pump(const Duration(seconds: 2));
-    expect(find.text("0"),findsOneWidget);
+    expect(find.text("0"), findsOneWidget);
   });
 }
